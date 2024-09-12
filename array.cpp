5776c27743ba3917f50dc55e6453a42d5c2a6209
Anjali Prajapati <160333990+anjali67c@users.noreply.github.com>
2024-09-12 19:08:58 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,18 @@
+// colection of similar kinds of data is called array
+// numeric array
+// numberic has three types:- 1 d , 2 d , multidimensional array.
+// associative array(anything except number)
+// to declare any array we add datatype, then arrayname and size or subscript in bracket.
+#include<iostream>
+using namespace std;
+int main()
+{
+    //datatype arrayname[size]
+    int cybrom[2];
+    cybrom[0]=90;
+    cybrom[1]=345;
+    cout<<cybrom<<"\n";// adress of 0 indexing
+    cout<<&cybrom[0]<<"\n";// address of 0 indexing
+    cout<<cybrom[0]<<"\n";// address of 10 indexing.
+    cout<<cybrom[1]<<"\n"// address f  indexing.
+}
